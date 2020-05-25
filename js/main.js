0f5b12e414fbdbf0a32fcedc7045858f4ab2a608
Lukas Wieser <lukas.luki.wieser@gmail.com>
2020-05-26 00:52:17 +0200

    create class point-cloud-svg, to create and drag points in an svg

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import { pointCloudSvg } from "./point-cloud-canvas.js";
+
+const pointCloudSmall = new pointCloudSvg("svg-small");
+const pointCloudBig = new pointCloudSvg("svg-big");
---
+++ js/point-cloud-canvas.js
@@ -0,0 +1,71 @@
+class pointCloudSvg {
+    constructor(svgID) {
+        this.svg = document.getElementById(svgID);
+        this.draggedElement = null;
+        this.preventNextMouseClick = false;
+
+        this.svg.addEventListener("click", (evt) => this.createPoint(evt) );
+        this.svg.addEventListener('mousemove', (evt) => this.drag(evt));
+        this.svg.addEventListener('mouseup', (evt) => this.endDrag(evt));
+    }
+
+    getAdjustedMouseCoords(evt){
+        const rect = this.svg.getBoundingClientRect();
+        
+        // get cursor Coordinates, relative to our svg-element
+        const cursorX = evt.clientX - rect.left;
+        const cursorY = evt.clientY - rect.top;
+    
+        // adjust the coordinates, so that they are betweeen 0 and 100
+        const x = Math.floor(cursorX / rect.width  * 100);
+        const y = Math.floor(cursorY / rect.height * 100);
+    
+        return { x: x, y: y };
+    }
+
+    // -- create point
+
+    createPoint(evt) {
+        if(this.preventNextMouseClick === true){
+            this.preventNextMouseClick = false;
+            return;
+          }
+
+        const coords = this.getAdjustedMouseCoords(evt);
+
+        let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        circle.setAttributeNS(null, "cx", coords.x);
+        circle.setAttributeNS(null, "cy", coords.y);
+        circle.setAttributeNS(null, "r", 1);
+        circle.setAttributeNS(null, "class", "cavas__star js-canvas__star");
+        circle.addEventListener('mousedown', (evt) => this.startDrag(evt));
+
+        this.svg.appendChild(circle);
+    }
+
+    // -- drag events
+
+    startDrag(evt) {
+        this.draggedElement = evt.target;
+    }
+
+    endDrag(evt){
+        // check if there even was a draggedElement
+        if(!this.draggedElement){ return; }
+
+        this.draggedElement = null;
+        this.preventNextMouseClick = true;
+    }
+
+    drag(evt){
+        // check if there even was a draggedElement
+        if(!this.draggedElement){ return; }
+
+        evt.preventDefault();
+        const coord = this.getAdjustedMouseCoords(evt);
+        this.draggedElement.setAttributeNS(null, "cx", coord.x);
+        this.draggedElement.setAttributeNS(null, "cy", coord.y);
+    }
+}
+
+export { pointCloudSvg };
