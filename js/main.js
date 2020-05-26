e80b6b63f9f50320c4c71dad130c5f216cb2e501
Lukas Wieser <lukas.luki.wieser@gmail.com>
2020-05-27 00:23:26 +0200

    add button to print the current points; add button to import a predefined dataset

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import { pointCloudSvg } from "./point-cloud-canvas.js";
+import { pointsSmall } from "../config/points-small.js";
+import { pointsBig } from "../config/points-big.js";
 
-const pointCloudSmall = new pointCloudSvg("svg-small", "bt-delete-svg-small");
-const pointCloudBig = new pointCloudSvg("svg-big", "bt-delete-svg-big");
+const pointCloudSmall = new pointCloudSvg("svg-small", "bt-delete-svg-small", "bt-import-small", pointsSmall, "bt-eval-small");
+const pointCloudBig = new pointCloudSvg("svg-big", "bt-delete-svg-big", "bt-import-big", pointsBig, "bt-eval-big");
--- js/point-cloud-canvas.js
+++ js/point-cloud-canvas.js
@@ -1,15 +1,45 @@
 class pointCloudSvg {
-    constructor(svgID, deleteAllButtonID) {
+    constructor(svgID, deleteAllButtonID, importButtonID, importData, evalButtonID) {
         this.draggedElement = null;
         this.preventNextMouseClick = false;
+        this.importData = importData;
 
+        // init DOM Elements:
         this.svg = document.getElementById(svgID);
         this.btDeleteAll = document.getElementById(deleteAllButtonID);
+        this.btEval = document.getElementById(evalButtonID);
+        this.btImport = document.getElementById(importButtonID);
 
+        // init Events:
         this.svg.addEventListener("click", (evt) => this.createPoint(evt) );
         this.svg.addEventListener('mousemove', (evt) => this.drag(evt));
         this.svg.addEventListener('mouseup', (evt) => this.endDrag(evt));
+
         this.btDeleteAll.addEventListener("click", () => this.deleteAllPoints());
+        this.btEval.addEventListener("click", () => this.evalPoints());
+        this.btImport.addEventListener("click", () => this.importPoints());
+    }
+    
+    evalPoints() {
+        let points = [];
+
+        var pointsHTML  = this.svg.getElementsByClassName("js-canvas__star");
+
+        for(var i = 0; i < pointsHTML.length; i++){
+            points.push({
+                x: pointsHTML[i].getAttributeNS(null, "cx"),
+                y: pointsHTML[i].getAttributeNS(null, "cy")
+            });
+        }
+      
+        console.log("points:");
+        console.log(points);
+    }
+
+    importPoints() {
+        for(let i = 0; i < this.importData.length; i++) {
+            this.createPointHTML(this.importData[i].x, this.importData[i].y);
+        }
     }
 
     getAdjustedMouseCoords(evt){
@@ -35,10 +65,13 @@ class pointCloudSvg {
           }
 
         const coords = this.getAdjustedMouseCoords(evt);
+        this.createPointHTML(coords.x, coords.y);
+    }
 
+    createPointHTML(x, y) {
         let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        circle.setAttributeNS(null, "cx", coords.x);
-        circle.setAttributeNS(null, "cy", coords.y);
+        circle.setAttributeNS(null, "cx", x);
+        circle.setAttributeNS(null, "cy", y);
         circle.setAttributeNS(null, "r", 1);
         circle.setAttributeNS(null, "class", "cavas__star js-canvas__star");
         circle.addEventListener('mousedown', (evt) => this.startDrag(evt));
